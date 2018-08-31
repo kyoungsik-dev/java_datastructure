2806dba009d6d5cfb2bd7f19754f295c7f2c540a
Kyoungsik Choi <kyoungsik.dev@gmail.com>
2018-08-31 08:58:30 +0000

    #19 File Input Phonebook

--- project/Main.java
+++ project/Main.java
@@ -1,13 +1,33 @@
 package project;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        System.out.println("Hello goorm!");
+        String [] name = new String [1000];
+        String [] number = new String [1000];
+        int n = 0;
+        try {
+            String fileName = "/workspace/java_datastructure/src/project/input.txt";
+            Scanner inFile;
+            inFile = new Scanner( new File(fileName) );
+            
+            while( inFile.hasNext() ) {
+                name[n] = inFile.next();
+                number[n] = inFile.next();
+                n++;
+            }
+            
+            inFile.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("No File");
+            System.exit(0);
+        }
+        
+        for(int i=0; i<n; i++) {
+            System.out.println(name[i] + ": " + number[i]);
+        }
     }
-
 }
